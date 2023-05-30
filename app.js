5a9c030ed2eabce8f407da739cd4d0800a9be670
gary909 <garywhite61@hotmail.com>
2023-05-30 12:24:57 +0200

    code

---
+++ app.js
@@ -0,0 +1,16 @@
+function inAscOrder(arr) {
+    // Code your algorithm here :)
+    
+    // Hmmm ... maybe we should try our luck out ...
+    // if (Math.random() > 0.5) {
+    //  return true;
+    // } else {
+    //  return false;
+    // }
+}
+
+console.log(inAscOrder([1, 2, 4, 7, 19])); // true
+console.log(inAscOrder([1, 2, 3, 4, 5])); // true
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20])); // false
+console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1])); // false
+
